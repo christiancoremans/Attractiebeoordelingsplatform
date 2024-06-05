121952d8a378ae16fc0f068e480e09363c5369da
Raymond klinken <124687674+Ducklord69@users.noreply.github.com>
2024-06-05 09:02:41 +0200

    extra map

--- attractiebeoordelingsplatform/resources/views/components/head.php
+++
@@ -1,8 +0,0 @@
-<?php require_once __DIR__.'/../../../config/conn.php'; ?>
-
-<meta charset="utf-8">
-<meta name="description" content="StoringApp voor technische dienst van DeveloperLand">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="icon" href="<?php echo $base_url; ?>/favicon.ico" type="image/x-icon">
-<link rel="stylesheet" href="<?php echo $base_url; ?>/attractiebeoordelingsplatform/public_html/css/normalize.css">
-<link rel="stylesheet" href="<?php echo $base_url; ?>/attractiebeoordelingsplatform/public_html/css/main.css">
